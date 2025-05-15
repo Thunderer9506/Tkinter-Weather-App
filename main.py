1cef7145b027a0a7b44d1e58d1165c71ed9bb098
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-05-15 09:40:37 +0530

    Completed the Project

--- main.py
+++ main.py
@@ -10,13 +10,10 @@ class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Weather App")
-        # self.geometry("500x500")
 
-        # Create a frame for the upper part of the UI
         upper_frame = ttk.Frame(self)
         upper_frame.pack(side='top', anchor='nw', padx=10, pady=10, fill='x')
 
-        # Place the widgets in the frame
         label = ttk.Label(upper_frame, text="Enter City: ")
         label.pack(side='left', padx=5, pady=5)
 
@@ -26,11 +23,9 @@ class GUI(tk.Tk):
         button = ttk.Button(upper_frame, text="Submit", command=self.display)
         button.pack(side='left', padx=5, pady=5)
 
-        # Add a spacer to separate the button from the labels
         topspacer = ttk.Label(self, text="")
         topspacer.pack(side='top', pady=5)
 
-        # Place the labels below each other
         self.temp = ttk.Label(self, text='Temperature: ')
         self.temp.pack(side='top', anchor='w', padx=15, pady=5)
 
@@ -43,19 +38,18 @@ class GUI(tk.Tk):
         self.windSpeed = ttk.Label(self, text='Wind Speed: ')
         self.windSpeed.pack(side='top', anchor='w', padx=15, pady=5)
 
-        topspacer = ttk.Label(self, text="")
-        topspacer.pack(side='bottom', pady=5)
+        bottomSpacer = ttk.Label(self, text="")
+        bottomSpacer.pack(side='bottom', pady=5)
 
         self.mainloop()
 
     def display(self):
         weather = Weather(self.entry.get().title())
         data = weather.fetch_weatherData()
-        self.temp['text'] = 'Temperature:'+data["main"]["temp"]
-        self.condition['text'] = 'Condition:'+data["weather"]["description"]
-        self.humidity['text'] = 'Humidity:'+data["main"]["humidity"]
-        self.windSpeed['text'] = 'Wind Speed:'+data["wind"]["2.44"]+"km"
-
+        self.temp['text'] = f'Temperature: {data["main"]["temp"]}C'
+        self.condition['text'] = f'Condition: {data["weather"][0]["description"]}'
+        self.humidity['text'] = f'Humidity: {data["main"]["humidity"]}%'
+        self.windSpeed['text'] = f'Wind Speed: {data["wind"]["speed"]} km/h'
 
 class Weather:
     def __init__(self,cityName):
@@ -66,8 +60,12 @@ class Weather:
         self.GeoCode = self.get_geocoords()
 
     def get_geocoords(self):
-        params = f"q={self.cityName}&appid={self.appid}"
-        get_geocode = requests.get(self.GEOURL+params)
+        params = {
+            'q':self.cityName,
+            'appid':self.appid,
+        }
+
+        get_geocode = requests.get(self.GEOURL,params)
         data = get_geocode.json()[0]
         return (data['lat'],data['lon'])
     
@@ -78,21 +76,9 @@ class Weather:
             'appid':self.appid,
             'units':"metric",
         }
-        # params = f"lat={self.GeoCode[0]}&lon={self.GeoCode[1]}&appid={self.appid}&units=metric"
-        get_geocode = requests.get(self.WeatherURL,params)
-        data = get_geocode.json()
+
+        get_weatherData = requests.get(self.WeatherURL,params)
+        data = get_weatherData.json()
         return data
 
-# get_geocode = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=Ghaziabad&appid=ec99aec66d6d231466250cf9fb84d219")
-
-# GET request to GeeksforGeeks
-# get_response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ec99aec66d6d231466250cf9fb84d219")
-
-# Printing only the status code
-# print(get_geocode.json()[0]['lat'],get_geocode.json()[0]['lon'])
-
-geocode = Weather("Ghaziabad")
-print(geocode)
-
-
-# GUI()
+GUI()
